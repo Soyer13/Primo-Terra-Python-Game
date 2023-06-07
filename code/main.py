138ede67a0a985009a556899cea685c15de6d9e1
Soyer13 <99680807+Soyer13@users.noreply.github.com>
2023-06-07 17:19:10 +0200

    Dodanie Porad Do Menu

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@ import pygame, sys
 from settings import *
 from level import Level
 from button import Button
+import random
 
 class Game:
 	
@@ -12,6 +13,7 @@ class Game:
 		pygame.display.set_caption('Primo Terra')
 		self.clock = pygame.time.Clock()
 		self.font = pygame.font.Font(UI_FONT,UI_FONT_SIZE)
+		self.font_advice = pygame.font.Font(UI_FONT,14)
 
 		self.level = Level()
 	
@@ -21,11 +23,16 @@ class Game:
 		self.MianBC_surf = pygame.transform.scale(self.MianBC_surf, (WIDTH, HEIGTH))
 		self.MainBC_rect = self.MianBC_surf.get_rect(topleft = (0,0))
 
-		
-  
-  
+		 #Tytuł 
 		PrimoTerra_surf = self.font.render(str('Primo Terra'),False,'#6AF468')
 		PrimowTerra_rect = PrimoTerra_surf.get_rect(center=(WIDTH / 2, HEIGTH/6))
+	
+		#Porady
+		i = random.randint(0, len(Advice_menu) -1)
+		Advice_surf = self.font_advice.render(str(Advice_menu[i]),False,'white')
+		Advice_rect = Advice_surf.get_rect(center=(WIDTH/2, HEIGTH/1.2))
+
+		#przyciski
 		Start_img = pygame.image.load('../graphics/buttons/Start.png').convert_alpha()
 		StartButton =Button(WIDTH / 2.3, HEIGTH/3.6, Start_img, 0.8)
 		Exit_img = pygame.image.load('../graphics/buttons/Koniec.png').convert_alpha()
@@ -42,7 +49,10 @@ class Game:
 			self.screen.blit(self.MianBC_surf,self.MainBC_rect)
 			#Menu Główne
 			if isStart == False:
+				#Wyświetlanie Tytułu
 				self.screen.blit(PrimoTerra_surf,PrimowTerra_rect )
+				#Wyświetlenie Porady
+				self.screen.blit(Advice_surf,Advice_rect )
 				if StartButton.draw(self.screen):
 					isStart = True
 				if ExitButton.draw(self.screen):
