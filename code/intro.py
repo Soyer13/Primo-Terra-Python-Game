f8e7fe877b665793f0f34a029435328b5d5b9eb4
Soyer13 <99680807+Soyer13@users.noreply.github.com>
2023-06-10 09:27:55 +0200

    Ekran Ładowania do gifa

--- code/intro.py
+++ code/intro.py
@@ -1,7 +1,7 @@
 from settings import *
 import pygame
 from PIL import Image
-def wyswietlintro():
+def wyswietlintro(screen):
     # Inicjalizacja biblioteki Pygame
     pygame.init()
 
@@ -15,14 +15,14 @@ def wyswietlintro():
             gif.seek(len(frames))
     except EOFError:
         pass
-
+    
+  
     main_sound = pygame.mixer.Sound('../audio/Together-to-the-Stars.mp3')
     main_sound.set_volume(MusicVolume)
     main_sound.play()
-    # Utworzenie okna o rozmiarach pierwszej klatki GIF
-    width, height = frames[0].size
-    screen = pygame.display.set_mode((width, height))
-
+    
+   
+    
     # Konwersja klatek na format zrozumiały dla Pygame
     pygame_frames = []
     for frame in frames:
@@ -36,19 +36,23 @@ def wyswietlintro():
     running = True
     clock = pygame.time.Clock()
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+        try:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
-        # Wyświetlanie bieżącej klatki na ekranie
-        screen.blit(pygame_frames[current_frame_index], (0, 0))
-        pygame.display.flip()
+            # Wyświetlanie bieżącej klatki na ekranie
+            screen.blit(pygame_frames[current_frame_index], (0, 0))
+            pygame.display.flip()
 
-        # Zwiększanie indeksu bieżącej klatki i cykliczne odtwarzanie animacji
-        current_frame_index = (current_frame_index + 1) % len(pygame_frames)
-
-        # Ustalenie liczby klatek na sekundę (FPS)
-        clock.tick(30)  # Możesz dostosować wartość FPS według własnych potrzeb
+            # Zwiększanie indeksu bieżącej klatki i cykliczne odtwarzanie animacji
+            current_frame_index = (current_frame_index + 1) % len(pygame_frames)
+            clock.tick(30)  # Możesz dostosować wartość FPS według własnych potrzeb
+        except IndexError:
+            running = False
+            main_sound.stop()
+            # Ustalenie liczby klatek na sekundę (FPS)
+            
 
     # Zakończenie programu
     pygame.quit()
